3407dc4b51aaeba60d2bde1f6db52cbe05c79d19
SilverFS <faizalsusetyo@gmail.com>
2022-04-05 15:40:43 +0200

    Fixed Create Notify

--- src/components/ProductCreate.js
+++ src/components/ProductCreate.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
     Create, useNotify, useRefresh, useRedirect, TextInput, SimpleForm
 } from 'react-admin';
+import RichTextInput from 'ra-input-rich-text';
 
 
 
-const ProductCreate = (props) => (
-    
-    /* this.state = {
-         notify: useNotify(),
-         refresh: useRefresh(),
-         redirect: useRedirect()
-    }
-    
+const ProductCreate = props => {
+    const notify = useNotify();
+    const refresh = useRefresh();
+    const redirect = useRedirect();
 
-    const onSuccess = ({ data }) => {
-        this.state.notify(`New user created! `);
-        redirect(`/products?filter=%7B"id"%3A${data.id}`);
+    const onSuccess = ({data}) => {
+        redirect(`/products/${data.id}`);
+        notify(`Product created!`);
         refresh();
-    }; */
+    };
 
     
-    /* return ( */
-        <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
+    return (
+        <Create {...props} title='Create new product' onSuccess={onSuccess}>
             <SimpleForm>
-                <TextInput disabled source='id' defaultValue='1' />
-                <TextInput source='name' />
-                <TextInput source='email' />
+                <TextInput source='title' />
+                <RichTextInput source='description' />
             </SimpleForm>
         </Create>
-    /* ); */
+    );
     
-);
+};
 
 export default ProductCreate
