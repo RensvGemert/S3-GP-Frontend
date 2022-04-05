486449fc265a64dca77f64e1f9d8b904e723cf5d
SilverFS <faizalsusetyo@gmail.com>
2022-04-05 15:40:11 +0200

    Added fetch options and params

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser, List } from 'react-admin';
+import { Admin, Resource, fetchUtils, ListGuesser, List } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import ProductList from './components/ProductList'
 import UserList from "./components/UserList";
@@ -7,8 +7,19 @@ import UserEdit from "./components/UserEdit";
 import ProductEdit from "./components/ProductEdit";
 import UserCreate from "./components/UserCreate";
 import ProductCreate from "./components/ProductCreate";
+import simpleRestProvider from 'ra-data-simple-rest';
 
-const dataProvider = jsonServerProvider('http://localhost:8080/api');
+const fetchJson = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    // add your own headers here
+    options.headers.set('Access-Control-Allow-Origin', '*');
+    return fetchUtils.fetchJson(url, options);
+}
+
+const dataProvider = jsonServerProvider('http://localhost:8080/api', fetchJson);
+
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
