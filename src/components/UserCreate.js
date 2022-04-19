d9378205a561d5bd1313f6661350c160afef565c
SilverFS <faizalsusetyo@gmail.com>
2022-04-19 15:39:02 +0200

    added notify + cleanup

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -1,36 +1,32 @@
 import React from 'react'
 import {
-    Create, useNotify, useRefresh, useRedirect, TextInput, SimpleForm
+    Create, useNotify, useRefresh, useRedirect, TextInput, NumberInput, SimpleForm
 } from 'react-admin';
+import RichTextInput from 'ra-input-rich-text';
 
 
 
-const UserCreate = (props) => (
-    
-    /* this.state = {
-         notify: useNotify(),
-         refresh: useRefresh(),
-         redirect: useRedirect()
-    }
-    
+const UserCreate = props => {
+    const notify = useNotify();
+    const refresh = useRefresh();
+    const redirect = useRedirect();
 
-    const onSuccess = ({ data }) => {
-        this.state.notify(`New user created! `);
-        redirect(`/users?filter=%7B"id"%3A${data.id}`);
+    const onSuccess = ({data}) => {
+        redirect(`/users/${data.id}`);
+        notify(`User created!`);
         refresh();
-    }; */
+    };
 
     
-    /* return ( */
-        <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
+    return (
+        <Create {...props} title='Create new user' onSuccess={onSuccess}>
             <SimpleForm>
-                <TextInput disabled source='id' />
                 <TextInput source='name' />
                 <TextInput source='email' />
             </SimpleForm>
         </Create>
-    /* ); */
+    );
     
-);
+};
 
 export default UserCreate
