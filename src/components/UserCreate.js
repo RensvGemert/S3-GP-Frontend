1aa9e265530dc97558784d0f0a5c9772b8378e26
SilverFS <faizalsusetyo@gmail.com>
2022-04-25 15:22:38 +0200

    Added query + dropdown for company

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-    Create, useNotify, useRefresh, useRedirect, TextInput, NumberInput, SimpleForm
+    Create, useNotify, useRefresh, useQuery, useRedirect, TextInput, SimpleForm, SelectInput
 } from 'react-admin';
-import RichTextInput from 'ra-input-rich-text';
 
 
 
@@ -17,12 +16,31 @@ const UserCreate = props => {
         refresh();
     };
 
+    const [company, setCustomers] = useState([]);
+    const { data: companies } = useQuery({
+        type: 'getList',
+        resource: 'company',
+        payload: {
+        pagination: { page: 1, perPage: 600 },
+        sort: { field: 'companyName', order: 'ASC' },
+        filter: {},
+        },
+    });
+
+
+    useEffect(() => {
+        if (companies)
+        setCustomers(companies.map((d) => ( { name: d.companyName, id: d.companyId } )));
+    }, [companies]);
+
     
     return (
         <Create {...props} title='Create new user' onSuccess={onSuccess}>
             <SimpleForm>
                 <TextInput source='name' />
                 <TextInput source='email' />
+                <TextInput source='password' />
+                <SelectInput source='companyId' choices={company} />
             </SimpleForm>
         </Create>
     );
