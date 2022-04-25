8718df58f56c229f1349f6f49b05cc71eedbff5c
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2022-04-25 19:52:07 +0200

    Dynamic Edit

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-    Edit, SimpleForm, TextInput, NumberInput
+    Edit, SimpleForm, TextInput, NumberInput, ArrayInput, SimpleFormIterator, SelectInput
 } from 'react-admin';
 import RichTextInput from 'ra-input-rich-text';
 
@@ -13,6 +13,17 @@ const ProductEdit = (props) => (
             <NumberInput source='discount' min={0} max={100} />
             <TextInput source='image' type='url' />
             <RichTextInput source='description' />
+            <ArrayInput source ='productField' >
+                    <SimpleFormIterator disableReordering>
+                        <TextInput source='productFieldName' label='Product field name' />
+                        <SelectInput source='productFieldType' label='Product field type' choices={[
+                            { id: '1', name: 'String' },
+                            { id: '2', name: 'Integer' },
+                            { id: '3', name: 'Boolean' },
+                        ]} />
+                        <TextInput source='productFieldValue' label='Product field value' />
+                    </SimpleFormIterator>
+                </ArrayInput>
         </SimpleForm>
     </Edit>
 );
