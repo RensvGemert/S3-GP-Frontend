9d2016b06e94a5d1ade5346854cd543c59e30342
Sander van Deurzen <s.vandeurzen@student.fontys.nl>
2022-05-24 09:46:28 +0200

    oops

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -2,35 +2,59 @@ import React from 'react'
 import {
     Create, TextInput, SimpleForm
 } from 'react-admin';
+import bcrypt from 'bcryptjs'
 
 
 
-const UserCreate = (props) => (
+// function postSave(password) {
+//     var rounds = 10
+
+//     bcrypt.hash(password, 10, (err, hash) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     console.log(hash)
+//     })
+//     return password;
+// };
+
+const UserCreate = (props) => {
     
     /* this.state = {
          notify: useNotify(),
          refresh: useRefresh(),
          redirect: useRedirect()
     }
-    
-
-    const onSuccess = ({ data }) => {
-        this.state.notify(`New user created! `);
-        redirect(`/users?filter=%7B"id"%3A${data.id}`);
-        refresh();
-    }; */
-
-    
-    /* return ( */
-        <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
-            <SimpleForm>
+    */
+   var hash;
+    function change(password){
+        var egg = document.getElementById('password').value; 
+        console.log(egg)
+        bcrypt.hash(egg, 10, (err, hash) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(hash);
+            console.log(document.getElementById('password').value);
+            document.getElementById('password').value = hash;
+            
+            })
+    }
+    const transform = data => ({
+        ...data,
+        password: `${document.getElementById('password').value}`,
+    });
+    return ( 
+        <Create {...props} title='Create new user'  ons>
+            <SimpleForm  onSubmit={change({})}>
                 <TextInput disabled source='id' />
                 <TextInput source='name' />
                 <TextInput source='email' />
-            </SimpleForm>
-        </Create>
-    /* ); */
-    
-);
+                <TextInput name='password' source='password'  />
+            </SimpleForm >
+        </Create >
+     ); 
+    }
 
 export default UserCreate
