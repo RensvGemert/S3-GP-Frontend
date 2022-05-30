74ff3e9dd2b1d9bf655e37c7907007ebf45d6a87
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2022-05-30 11:17:46 +0200

    userCreate & -Edit updated
    userCreate & -Edit mogelijkheden gesplitst voor admin of geen admin

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -1,40 +1,39 @@
 import React from 'react'
 import {
-    Create, TextInput, SimpleForm
+    Create, TextInput, SimpleForm, ReferenceInput, SelectInput
 } from 'react-admin';
-import userCompanyId from "./api-redirects/userCompanyId"
+const companyId = localStorage.getItem('companyId');
 
-
-
-const UserCreate = (props) => (
-    
-    /* this.state = {
-         notify: useNotify(),
-         refresh: useRefresh(),
-         redirect: useRedirect()
+function UserCreate(props) {
+    if(companyId === '1') 
+    {
+        return (
+            <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
+                <SimpleForm>
+                    <TextInput source='name' />
+                    <TextInput source='email' type="email" />
+                    <TextInput source='password' type="password" />
+                    <ReferenceInput source='companyId' reference="companies" label="Company">
+                        <SelectInput optionText="name" />
+                    </ReferenceInput>
+                </SimpleForm>
+            </Create>
+        );
+    }
+    else
+    {
+        return (
+            <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
+                <SimpleForm>
+                    <TextInput source='name' />
+                    <TextInput source='email' type="email" />
+                    <TextInput source='password' type="password" />
+                    <TextInput disabled source='companyId' defaultValue={localStorage.getItem(`companyId`)} />
+                </SimpleForm>
+            </Create>
+        );
     }
     
-
-    const onSuccess = ({ data }) => {
-        this.state.notify(`New user created! `);
-        redirect(`/users?filter=%7B"id"%3A${data.id}`);
-        refresh();
-    }; */
-
     
-
-
-    /* return ( */
-        <Create {...props} title='Create new user' /* onSuccess={onSuccess} */>
-            <SimpleForm>
-                <TextInput source='name' />
-                <TextInput source='email' type="email" />
-                <TextInput source='password' type="password" />
-                <TextInput disabled source='companyId' defaultValue={localStorage.getItem(`companyId`)} />
-            </SimpleForm>
-        </Create>
-    /* ); */
-    
-);
-
+}
 export default UserCreate
--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import {
-    Edit, SimpleForm, TextInput, NumberInput
+    Edit, SimpleForm, TextInput, ReferenceInput, SelectInput
 } from 'react-admin';
+const companyId = localStorage.getItem('companyId');
 
-const UserEdit = (props) => (
-    <Edit {...props}>
-        <div>
-        <SimpleForm>
-            <TextInput disabled source='id' />
-            <TextInput source='name' />
-            <TextInput source='email' type="email" />
-            <TextInput source='password' type="password" />
-            <TextInput disabled source='companyId' />
-        </SimpleForm>
-        </div>   
-    </Edit>
-);
+function UserEdit(props) {
+    if(companyId === '1') 
+    {
+        return (
+            <Edit {...props} title='Edit user' /* onSuccess={onSuccess} */>
+                <SimpleForm>
+                    <TextInput source='name' />
+                    <TextInput source='email' type="email" />
+                    <TextInput source='password' type="password" />
+                    <ReferenceInput source='companyId' reference="companies" label="Company">
+                        <SelectInput optionText="name" />
+                    </ReferenceInput>
+                </SimpleForm>
+            </Edit>
+        );
+    }
+    else
+    {
+        return (
+            <Edit {...props} title='Edit user' /* onSuccess={onSuccess} */>
+                <SimpleForm>
+                    <TextInput source='name' />
+                    <TextInput source='email' type="email" />
+                    <TextInput source='password' type="password" />
+                    <TextInput disabled source='companyId' defaultValue={localStorage.getItem(`companyId`)} />
+                </SimpleForm>
+            </Edit>
+        );
+    }
+    
+    
+}
 
 export default UserEdit
